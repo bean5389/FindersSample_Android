24c0b97690bca76f1064636fcfb0ed83e3ae885c
“Bean” <bean5389@gmail.com>
2019-01-20 21:40:32 +0900

    Initial commit

---
+++ app/src/main/java/me/bean5389/finderssample/IntroActivity.java
@@ -0,0 +1,14 @@
+package me.bean5389.finderssample;
+
+import android.os.Bundle;
+
+import me.bean5389.finderssample.base.BaseActivity;
+
+public class IntroActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
---
+++ app/src/main/java/me/bean5389/finderssample/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package me.bean5389.finderssample.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bean on 20/01/2019.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
